4f1b7f3d7647cd7d8460f527603df673bbf877e4
EleanorWho <huj1@tcd.ie>
2025-06-02 15:13:03 +0100

    deleted script

--- app/test_api.py
+++
@@ -1,6 +0,0 @@
-from requests import get, post, put, delete, HTTPError
-from pymongo import MongoClient
-
-uri = "mongodb+srv://admin:bnoasojGXBwJqPUa@goemotion.04rdzbd.mongodb.net/?retryWrites=true&w=majority&appName=GoEmotion"
-client = MongoClient(uri)
-print(client.list_database_names())
